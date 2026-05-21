7bb7e96ec59037d2edeba0f9880f9cab140be169
Bedirhan Büyükduman <b.buyukduman@gmail.com>
2026-05-21 10:56:49 +0300

    basePath kaldırıldı

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,8 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
-  // Set basePath to your GitHub repo name when deploying to GitHub Pages.
-  // Change 'tmu-usul' to your actual repository name.
-  basePath: isProd ? '/usulen' : '',
   images: {
     unoptimized: true,
   },
